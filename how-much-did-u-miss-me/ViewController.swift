5773d0080d74cf0fa535d538dc43cfabaef721e5
harungokcegoz <harungokcegoz92@gmail.com>
2023-03-08 10:36:29 +0100

    version 2 is done

--- how-much-did-u-miss-me/ViewController.swift
+++ how-much-did-u-miss-me/ViewController.swift
@@ -1,72 +1,93 @@
-//
-//  ViewController.swift
-//  how-much-did-u-miss-me
-//
-//  Created by Harun Gökçegöz on 07/03/2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+ 
+    var emojiImages = ["emoji0", "emoji1", "emoji2", "emoji3", "emoji4", "emoji5", "emoji6", "emoji7", "emoji8", "emoji9"]
+    var randomNumber = 3
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var progressLabel: UILabel!
+    @IBOutlet var dateTextField: UITextField!
     
-    var progressClass: Progress!
-    let emojiImages = ["emoji0", "emoji10", "emoji20", "emoji30", "emoji40", "emoji50", "emoji60", "emoji70", "emoji80", "emoji90"]
-    let choices = ["We had a phone call(10 points)", "We had a video call(20 points)", "We finally met(100 points)", "I sent a photo(50 points)"]
-    var timer: Timer?
+  
     
 
-    @IBOutlet var emojiImageView: UIImageView!
-    @IBOutlet var progressView: UIProgressView!
-    @IBOutlet var percentageLabel: UILabel!
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        // Get the selected date from the date picker
+        guard let selectedDate = dateTextField.text, !selectedDate.isEmpty else {
+            // Show an error message if the date field is empty
+            let alert = UIAlertController(title: "Error", message: "Please select a date and time", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+            return
+        }
+        
+        // Create a date formatter to convert the selected date to a Date object
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        
+        guard let targetDate = dateFormatter.date(from: selectedDate) else {
+            // Show an error message if the date field is not in the correct format
+            let alert = UIAlertController(title: "Error", message: "Please enter a valid date and time", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+            return
+        }
+        
+        // Calculate the time interval between the current date and the target date
+        let currentDate = Date()
+//        let timeInterval = targetDate.timeIntervalSince(currentDate)
+        let timeInterval = currentDate.timeIntervalSince(targetDate)
+        let hours = Int(timeInterval / 3600)
+        
+        // Calculate the progress value
+        var progressValue: Float = 0.0
+        
+        // Set the progress value to 0 if the time interval is negative
+        if hours > 100 {
+            progressValue = 100.0
+        } else {
+            progressValue = (Float(hours))
+        }
+        
+        // Update the progress bar and label
+        progressBar.progress = progressValue
+        progressLabel.text = "\(Int(hours))%"
+      
+        getRandomNumber()
+        print(randomNumber)
+        imageView.image = UIImage(named: emojiImages[randomNumber])
+        
+    }
 
-    @IBAction func phoneCall(_ sender: UIButton) {
-        progressClass.updateProgress(by: -10.0)
-    }
-    
-    @IBAction func videoCall(_ sender: UIButton) {
-        progressClass.updateProgress(by: -20.0)
-    }
-    
-    @IBAction func met(_ sender: UIButton) {
-        progressClass.resetProgress()
-    }
-    
-    
-    @IBAction func photo(_ sender: UIButton) {
-        progressClass.updateProgress(by: -50.0)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        progressClass = Progress(viewController: self)
-        updateProgressView()
-        startTimer()
+        imageView.image = UIImage(named: emojiImages[randomNumber])
+        progressBar.progress = 0.0
+        progressLabel.text = "0%"
+        dateTextField.delegate = self
     }
     
-    func updateProgressView() {
-        let firstImageName = emojiImages[0]
-        let firstImage = UIImage(named: firstImageName)
-        emojiImageView.image = firstImage
-        let progressPercentage = progressClass.progressPercentage
-        let index = Int(progressPercentage / 10.0)
-        emojiImageView.image = UIImage(named: emojiImages[index])
-        progressView.setProgress(Float(progressPercentage / 100.0), animated: true)
-        percentageLabel.text = "\(Int(progressPercentage))%"
-    }
-        
-    func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(updateProgress), userInfo: nil, repeats: true)
+    @objc func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
-    func stopTimer() {
-        timer?.invalidate()
-        timer = nil
-    }
-    
-    @objc func updateProgress() {
-        progressClass.updateProgress(by: 1.0)
-        updateProgressView()
+    func getRandomNumber(){
+        randomNumber = Int.random(in: 0...9)
     }
 
 }
 
+extension Date {
+    init?(from dateString: String, format: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        if let date = dateFormatter.date(from: dateString) {
+            self = date
+        } else {
+            return nil
+        }
+    }
+}
